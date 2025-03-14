439028f26279c88cd5bfabebd87c4aca4c90fb41
usuario <usuario@pc20-003>
2025-03-14 13:53:29 +0100

    Refactorización 6: Changing a Method's Signature

--- edu/uhu/monopoly/GameboardTest.java
+++ edu/uhu/monopoly/GameboardTest.java
@@ -43,8 +43,8 @@ public class GameboardTest extends TestCase {
 		gameBoard.addCell(cell1);
 		gameBoard.addCell(cell2);
 		gameBoard.addCell(cell3);
-		assertEquals(2, gameBoard.getPropertyNumberForColor("blue"));
-		assertEquals(1, gameBoard.getPropertyNumberForColor("green"));
+		assertEquals(2, gameBoard.getPropertyNumberForColor("blue", null));
+		assertEquals(1, gameBoard.getPropertyNumberForColor("green", null));
 	}
 	
 	public void testQueryCell() {
